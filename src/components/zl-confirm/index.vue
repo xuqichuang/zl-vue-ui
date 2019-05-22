d2c907fea8eaa2be3be1343e8105363240cd3954
徐其闯 <qichuan.xu@ly.com>
2019-05-22 13:52:00 +0800

    confirm

---
+++ src/components/zl-confirm/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class='container'>
+
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  computed: {
+
+  },
+  data() {
+    return{
+
+    }
+  },
+  watch: {
+
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+  props: {
+
+  },
+  destroyed() {
+
+  },
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
